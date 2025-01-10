c3f958c0785bc3b3b49d99ebf9bba05f7934fb44
RAHIL SHA <48413829+Rahilsha25@users.noreply.github.com>
2025-01-10 19:02:33 +0530

    Add files via upload

---
+++ assets/js/contactForm.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('submitButton');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_usk38ym';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Email Sent Successfully!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
